0c2c0917ecb7bfa9e75973392775eb226e492491
srenglaolengg <srenglaoleng969@gmail.com>
2025-09-14 13:24:17 +0700

    fix: Update Google Maps embed link
    Replaced the old Google Maps embed URL with a new `maps.app.goo.gl` link. This ensures the map displayed on the contact page is accurate and functional.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -59,7 +59,7 @@ const Contact: React.FC<ContactProps> = ({ language }) => {
             </div>
             <div className="lg:w-1/2 h-80 lg:h-auto rounded-lg shadow-lg overflow-hidden">
                 <iframe 
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.43123456789!2d105.3512345!3d11.5912345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTPCsDQ5JzIzLjQiTiAxMDXCsDIxJzE1LjYiRQ!5e0!3m2!1sen!2skh!4v1625234567890!5m2!1sen!2skh" 
+                    src="https://maps.app.goo.gl/y3wpFEJ1im6fsYra6" 
                     width="100%" 
                     height="100%" 
                     style={{ border: 0 }} 
