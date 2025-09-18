0e37b49c11d238448c561a4b58fb3111083ec400
srenglaolengg <srenglaoleng969@gmail.com>
2025-09-18 20:02:49 +0700

    feat: Add admin UID to constants
    Adds the Firebase Authentication User ID (UID) for the administrator to the `ADMIN_U_IDS` constant in `constants.ts`. This enables granting admin privileges to the specific user.

--- constants.ts
+++ constants.ts
@@ -3,4 +3,4 @@
 // To grant a user admin privileges, add their Firebase Authentication User ID (UID)
 // to this array. You can find a user's UID in the Firebase Console under
 // Authentication > Users.
-export const ADMIN_U_IDS = ['YOUR_ADMIN_FIREBASE_UID_HERE'];
+export const ADMIN_U_IDS = ['cy7nCO3gFnPXCuZ8pGrGYNvWx2B3'];
